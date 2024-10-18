4e479319142a55bf3a9943153660784a0b8085a6
Nevin Manimaran <nevin@ucsb.edu>
2024-10-18 12:15:59 -0700

    Initial Commit

---
+++ hw2/hw2App.swift
@@ -0,0 +1,17 @@
+//
+//  hw2App.swift
+//  hw2
+//
+//  Created by Nevin Manimaran on 10/18/24.
+//
+
+import SwiftUI
+
+@main
+struct hw2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
